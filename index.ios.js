3ce1564fe08baae5809317f85f6cc31892855605
Daniel André <dap1995@gmail.com>
2016-09-21 11:08:19 -0300

    Return register to frida instead of detinder

--- index.ios.js
+++ index.ios.js
@@ -8,4 +8,4 @@ import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
 
 import App from './App/container/';
-AppRegistry.registerComponent('detinder', () => App);
+AppRegistry.registerComponent('frida', () => App);
