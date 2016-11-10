06d5add9318661f3ce2fdd100f19b86448503108
Daniel André <dap1995@gmail.com>
2016-11-10 19:47:14 -0200

    Removed unused code

--- App/nav/index.js
+++ App/nav/index.js
@@ -21,30 +21,18 @@ export default class Nav extends Component {
   }
 
   render() {
-    if (this.props.routeId === 0){
       return (
         <View style={{ backgroundColor: '#2192d8', flexDirection: 'row' }}>
-        <Icon
-          name='perm-identity' color='white' />
-        <Text style={{paddingTop: 15, fontSize: 25, color: 'white'}}>{this.props.routes[this.props.routeId].title}</Text>
-        </View>
-      )
-    } else {
-      return (
-        <View style={{ backgroundColor: '#2192d8', flexDirection: 'row' }}>
+
         <TouchableOpacity onPress={() => {
-          if (this.props.routeId === 0) {
-            this.props.navigator.push(this.props.routes[1])
-          } else {
             this.props.navigator.pop();
-          }
-        }}>
+          }}>
         <Icon
           name='backspace' color='white' />
         </TouchableOpacity>
+
         <Text style={{paddingTop: 15, fontSize: 25, color: 'white'}}>{this.props.routes[this.props.routeId].title}</Text>
         </View>
       )
     }
   }
-}
