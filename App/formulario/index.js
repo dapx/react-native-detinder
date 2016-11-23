cc7ba352eed8482d01641b3176ae6b085bbb665f
Daniel André <dap1995@gmail.com>
2016-11-23 11:40:45 -0200

    Improved formulario

--- App/formulario/index.js
+++ App/formulario/index.js
@@ -12,20 +12,37 @@ import {
 import Nav from '../nav/index';
 import Perguntas from '../caracteristicas/perguntas';
 
-const char = {
-  tatuagem: false,
-  adornos: false,
-  altura: 1
-}
-
 export default class Form extends Component {
 
   constructor(props){
   super(props);
   this.state = {
-      tatuagem: char.tatuagem,
-      adornos: char.adornos,
-      altura: char.altura,
+      detento: {
+        foto_arq: null,
+        ind_nm: null,
+        ind_alcunha: null,
+        ind_nm_mae: null,
+        ind_rg: null,
+        ind_rguf: null,
+        ind_cpf: null,
+        ind_dtnasc: null,
+        ind_fone: null,
+        ind_altura: null,
+        ind_tp_tatoo: null,
+        ind_ds_tatoo: null,
+        ind_tp_scar: null,
+        ind_ds_scar: null,
+        ind_obs: null,
+        indcor_ds: null,
+        indcabelo_ds: null,
+        indolhos_ds: null,
+        log_nm: null,
+        log_cep: null,
+        bai_nm: null,
+        cid_nm: null,
+        log_nr: null,
+        ind_complemento: null
+      }
     };
   }
 
@@ -35,7 +52,7 @@ export default class Form extends Component {
 
   render() {
     return (
-      <Perguntas navigator={this.props.navigator} routes={this.props.routes}/>
+      <Perguntas detento={this.state.detento} navigator={this.props.navigator} routes={this.props.routes}/>
     );
   }
 }
