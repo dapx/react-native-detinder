cd5b761ff7d0fc824392c1523a638433ca1ffa74
Daniel André <dap1995@gmail.com>
2016-11-30 14:36:01 -0200

    Improved route navigation

--- App/container/index.js
+++ App/container/index.js
@@ -4,62 +4,81 @@ import {
   StyleSheet,
   Text,
   View,
+  ScrollView,
   Image,
   Navigator,
   TouchableOpacity
 } from 'react-native';
 
+import Dimensions from 'Dimensions';
+var windowSize = Dimensions.get('window');
 import SwipeWrapper from '../swipe/index';
 import Nav from '../nav/index';
 import Form from '../formulario/index';
+import Form2 from '../formulario/index2';
 import StartPage from '../start/index';
 import Cadastro from '../cadastro/index';
 const routes = [
   {id: 0, title: "SignUp"},
   {id: 1, title: "Dados da Vitima"},
   {id: 2, title: "Perfil Suspeito"},
-  {id: 3, title: "Possíveis Suspeitos"}
+  {id: 3, title: "Perguntas"},
+  {id: 4, title: "Possíveis Suspeitos"}
 ]
 
 export default class NavigatorApp extends Component {
 
   renderScene(route, navigator){
-    if (route.id === 0){
+    switch (route.id){
+      case 0:
       return (
         <View style={styles.container}>
-        <Image source={{uri: "https://s-media-cache-ak0.pinimg.com/564x/8d/44/da/8d44da5b9f38ba16d7d820c461fa1328.jpg"}} style={{ flex: 1, width: null, height: null, alignSelf: 'stretch' }}>
+        <Image source={{uri: "https://s-media-cache-ak0.pinimg.com/564x/8d/44/da/8d44da5b9f38ba16d7d820c461fa1328.jpg"}} style={{ flex: 1, width: windowSize.width, height: windowSize.height, position: 'absolute', left: 0, top: 0 }}>
         <StartPage navigator={navigator} routes={routes} routeId={route.id}/>
         </Image>
         </View>
       )
-    } else if (route.id === 1){
+      break;
+      case 1:
       return (
         <View>
         <Nav navigator={navigator} routeId={route.id} routes={routes} />
         <Cadastro navigator={navigator} routes={routes} routeId={route.id}/>
         </View>
       )
-    } else if (route.id === 2){
+      break;
+      case 2:
+      return (
+        <ScrollView>
+        <Nav navigator={navigator} routeId={route.id} routes={routes} />
+        <Form navigator={navigator} routes={routes} routeId={route.id}/>
+        </ScrollView>
+      )
+      break;
+      case 3:
       return (
         <View>
         <Nav navigator={navigator} routeId={route.id} routes={routes} />
-        <Form navigator={navigator} routes={routes} routeId={route.id}/>
+        <Form2 navigator={navigator} routes={routes} routeId={route.id}/>
         </View>
       )
-    } else if (route.id === 3){
+      break;
+      case 4:
       return (
         <View>
         <Nav navigator={navigator} routes={routes} routeId={route.id} />
         <SwipeWrapper navigator={navigator} routes={routes} routeId={route.id} />
         </View>
       )
-    } else {
+      break;
+      default:
       return (
         <View>
         <Nav navigator={navigator} routes={routes} routeId={route.id} />
         <SwipeWrapper navigator={navigator} routes={routes} routeId={route.id} />
         </View>
       )
+      break;
     }
   }
 
@@ -79,7 +98,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#ffffff',
+    backgroundColor: 'transparent',
   },
   card: {
     alignItems: 'center',
